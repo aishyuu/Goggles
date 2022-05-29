15cd7f8b73f7c23f3b83bcf1f06cbb89b60f0d5c
Frycen <ivanmatias1331@gmail.com>
2022-05-29 06:40:47 -0700

    added basic routes styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 import {Footer} from './components/Footer'
 import {Navbar} from './components/Navbar'
-import {Routes} from './components/Routes'
+import {Routing} from './components/Routing'
 
 function App() {
   const [darkTheme, setDarkTheme] = useState(false)
@@ -11,7 +11,7 @@ function App() {
     <div className={darkTheme ? 'dark' : ''}>
       <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 black min-h-screen'>
         <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
-        <Routes />
+        <Routing />
         <Footer />
       </div>
     </div>
